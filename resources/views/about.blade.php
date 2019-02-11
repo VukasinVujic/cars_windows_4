b72090fddbf3be99bc3d4f8059bc76248ce727c7
VukasinVujic <vukasinvujic021@gmail.com>
2019-02-11 12:28:22 +0100

    Tura 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    welcome to Cars Project
+                    About us    
                 </div>
 
                 <div class="links">
