9bf98fe797520578adfa87cce319b35c4b91e0e4
VukasinVujic <vukasinvujic021@gmail.com>
2019-02-11 13:46:03 +0100

    addding layouts

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.master')
 
-    <h1>INDEX BLADE FILE</h1>
+@section('title')
 
-    <ul>
+INDEX NASLOV
+    
+@endsection
+
+
+@section('sastav')
+
+
+<ul>
         @foreach ($cars as $car)
         
         <li><a href="{{ 'http://localhost/VIVIFY/napredni/Laravel/Predavanje_3/radOdKuce/Pon_11_02/cars_windows_4/public/cars/' . $car->id }}">{{ $car->title }}</a></li>
@@ -22,6 +22,16 @@
     </ul>
 
 
+
+@endsection
+
+
+
+
+
+
+
+
+
+
     
-</body>
-</html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,17 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.master');
 
-    <h1>SHOW BLADE FILE</h1>
+@section('sastav')
+  
+<h1>SHOW BLADE FILE</h1>
     <br>
     <h2>{{ $car->title }}</h2>
 
-    
-</body>
-</html>
+
+@endsection
+
+
+
